742f46d61f4f8676cd00b2b1a75db57fffdae7c2
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-03-09 09:28:44 +0300

    task-1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+// console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+// galleryContainer.addEventListener("click", onLinkClick);
+console.log(galleryItems);
+
+function createGalleryMarkup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+    alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
 
 console.log(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
