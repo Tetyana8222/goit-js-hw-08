fd70f755e78e421fa91d6b196ff35bb4adf2c856
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-03-09 19:33:20 +0300

    task 2 in progress

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+function onPlay(seconds) {
+  localStorage.setItem('videoplayer-current-time', 'seconds');
+}
+
+player.on('timeupdate', _throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,6 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('#feedback-form');
+const inputRef = document.querySelector('input');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+localStorage.setItem('feedback-form-state', JSON.stringif({ email, message }));
